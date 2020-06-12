b64851f88c422fb412af57d6856bfbdb0c07ab05
andreuhh <57524903+andreuhh@users.noreply.github.com>
2020-06-12 08:37:08 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+// import pages
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail';
+import Error from './pages/Error';
+
+export default function App() {
+  return <Router>app component</Router>;
+}
