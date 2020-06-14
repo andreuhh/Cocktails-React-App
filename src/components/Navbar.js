f8eedf53a6039b5075f1abad1afd1522d898bc19
andreuhh <57524903+andreuhh@users.noreply.github.com>
2020-06-14 16:46:10 +0200

    ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from '../logo.svg';
+//import logo from '../logo.svg';
 
 export default function Navbar() {
   return <nav className="navbar">
